45feff9b47983ef05f542f03127880c98d483ebe
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-02-03 10:53:34 +0200

    readability improvements

--- Algorithms/treeTraversal.cpp
+++ Algorithms/treeTraversal.cpp
@@ -1,5 +1,6 @@
 // this program implements three types of tree traversal: preorder, inorder, and
 // postorder.
+// the code is (hopefully) self-explanatory.
 
 #include <iostream>
 
@@ -9,32 +10,24 @@ struct Node {
     Node* m_right{nullptr};
 };
 
-Node* newNode(const int data) {
-    Node* temp{new Node{data}};
-    return temp;
-}
-
-// displaying the node first, then going to the left one, then to the right one
 void preorderTraversal(const Node* root) {
-    if (root) {
+    if (root != nullptr) {
         std::cout << root->m_data << "->";
         preorderTraversal(root->m_left);
         preorderTraversal(root->m_right);
     }
 }
 
-// going to the leftmost node first, displaying it, then going to the right one
 void inorderTraversal(const Node* root) {
-    if (root) {
+    if (root != nullptr) {
         inorderTraversal(root->m_left);
         std::cout << root->m_data << "->";
         inorderTraversal(root->m_right);
     }
 }
 
-// going to the leftmost node, then to the right one, then displaying it
 void postorderTraversal(const Node* root) {
-    if (root) {
+    if (root != nullptr) {
         postorderTraversal(root->m_left);
         postorderTraversal(root->m_right);
         std::cout << root->m_data << "->";
@@ -42,18 +35,19 @@ void postorderTraversal(const Node* root) {
 }
 
 int main() {
-    Node* root{newNode(11)};
-    root->m_left = newNode(7);
-    root->m_right = newNode(5);
-    root->m_left->m_left = newNode(13);
-    root->m_left->m_right = newNode(19);
-    root->m_left->m_left->m_left = newNode(1);
-    root->m_right->m_left = newNode(15);
-    root->m_right->m_right = newNode(6);
+    Node* root{new Node{11}};
+    root->m_left = new Node{7};
+    root->m_right = new Node{5};
+    root->m_left->m_left = new Node{13};
+    root->m_left->m_right = new Node{19};
+    root->m_left->m_left->m_left = new Node{1};
+    root->m_right->m_left = new Node{15};
+    root->m_right->m_right = new Node{6};
+    std::cout << "Preorder traversal: ";
     preorderTraversal(root);
-    std::cout << '\n';
+    std::cout << "\nInorder traversal: ";
     inorderTraversal(root);
-    std::cout << '\n';
+    std::cout << "\nPostorder traversal: ";
     postorderTraversal(root);
     std::cout << '\n';
 }
