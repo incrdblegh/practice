671cdfb290223ae3dd78ddbeb53437c8930538b3
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-03-04 20:49:27 +0200

    add ranged for-loop, check for empty argument

--- Algorithms/bracesCheck.cpp
+++ Algorithms/bracesCheck.cpp
@@ -1,28 +1,35 @@
-// this program implements a brace checker. 
+// this program implements a brace checker.
 
 #include <iostream>
 #include <stack>
 
 bool bracesCheck(const std::string& braces) {
+    if (braces.empty()) {
+        return false;
+    }
     std::stack<char> checker;
 
-    for (unsigned int i{0}; i < braces.length(); ++i) {
+    for (const auto& brace : braces) {
         if (checker.empty()) {
-            checker.push(braces[i]);
-        } else if ((checker.top() == '(' && braces[i] == ')') ||
-                   (checker.top() == '{' && braces[i] == '}') ||
-                   (checker.top() == '[' && braces[i] == ']')) {
+            checker.push(brace);
+        } else if ((checker.top() == '(' && brace == ')') ||
+                   (checker.top() == '{' && brace == '}') ||
+                   (checker.top() == '[' && brace == ']')) {
             checker.pop();
         } else {
-            checker.push(braces[i]);
+            checker.push(brace);
         }
     }
     return checker.empty();
 }
 
 int main() {
-    std::cout << bracesCheck("[{()}]") << '\n';
+    std::cout << bracesCheck("[{()}]") << '\n'; // the only true case
+    std::cout << bracesCheck("") << '\n';       // the rest are false
+    std::cout << bracesCheck(" ") << '\n';
+    std::cout << bracesCheck("     ") << '\n';
     std::cout << bracesCheck("[{[({(())}]") << '\n';
     std::cout << bracesCheck("[{((}[}[()]]))}]") << '\n';
     std::cout << bracesCheck("[{]}") << '\n';
+    std::cout << bracesCheck("gfdoqGSHOG \n5435`~':^#$)") << '\n';
 }
