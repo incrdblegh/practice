b99d089c087836749cf458892c7a7377e2526e17
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-05-13 20:19:26 +0300

    add singleton pattern

---
+++ Design Patterns/singleton.cpp
@@ -0,0 +1,25 @@
+class Singleton {
+  private:
+    Singleton() = default;
+
+  public:
+    static Singleton& get_instance() {
+        // this doesn't cause memory leaks, 'new' will be called only once.
+        // this approach is also thread-safe, and doesn't require explicit synchronization
+        static Singleton* instance = new Singleton;
+        return *instance;
+        // no need for explicit deallocation, the resource is freed when the program finishes running
+    }
+
+    // explicitly deleting copy and move operations 
+    Singleton(const Singleton&) = delete;
+    Singleton(Singleton&&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+    Singleton& operator=(Singleton&&) = delete;
+};
+
+// static memory allocation might be preferred if the resource held by the singleton is trivial
+// static Singleton& get_instance() {
+//     static Singleton instance;
+//     return instance;
+// }
