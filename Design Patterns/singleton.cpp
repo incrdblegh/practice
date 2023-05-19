e8be2ef301896925074273ec07b1b5b1f5706caf
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-05-19 19:56:04 +0300

    singleton solution cleanup

--- Design Patterns/singleton.cpp
+++ Design Patterns/singleton.cpp
@@ -1,25 +1,14 @@
-class Singleton {
-  private:
-    Singleton() = default;
-
+class Singleton final {
   public:
     static Singleton& getInstance() {
-        // this doesn't cause memory leaks, 'new' will be called only once.
-        // this approach is also thread-safe, and doesn't require explicit synchronization
         static Singleton* instance = new Singleton;
         return *instance;
-        // no need for explicit deallocation, the resource is freed when the program finishes running
     }
-
-    // explicitly deleting copy and move operations 
     Singleton(const Singleton&) = delete;
     Singleton(Singleton&&) = delete;
     Singleton& operator=(const Singleton&) = delete;
     Singleton& operator=(Singleton&&) = delete;
-};
 
-// static memory allocation might be preferred if the resource held by the singleton is trivial
-// static Singleton& get_instance() {
-//     static Singleton instance;
-//     return instance;
-// }
+  private:
+    Singleton() = default;
+};
