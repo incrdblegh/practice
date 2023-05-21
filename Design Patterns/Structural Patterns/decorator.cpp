28463778384ead160f9679c28856fd6f8d0e80c7
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-05-21 12:45:16 +0300

    update decorator solution with best practices

--- Design Patterns/Structural Patterns/decorator.cpp
+++ Design Patterns/Structural Patterns/decorator.cpp
@@ -5,31 +5,31 @@
 class Beverage {
   public:
     virtual ~Beverage() = default;
-    virtual std::string getDescription() = 0;
-    virtual int getCost() = 0;
+    [[nodiscard]] virtual std::string getDescription() const = 0;
+    [[nodiscard]] virtual int getCost() const = 0;
 };
 
 class Coffee : public Beverage {
   public:
-    std::string getDescription() override { return "coffee"; }
-    int getCost() override { return 20; }
+    std::string getDescription() const override { return "coffee"; }
+    int getCost() const override { return 20; }
 };
 
 class Tea : public Beverage {
   public:
-    std::string getDescription() override { return "tea"; }
-    int getCost() override { return 15; }
+    std::string getDescription() const override { return "tea"; }
+    int getCost() const override { return 15; }
 };
 
 class Juice : public Beverage {
   public:
-    std::string getDescription() override { return "juice"; }
-    int getCost() override { return 18; }
+    std::string getDescription() const override { return "juice"; }
+    int getCost() const override { return 18; }
 };
 
 class Addon : public Beverage {
   public:
-    Addon(std::unique_ptr<Beverage> beverage) : m_beverage{std::move(beverage)} {}
+    explicit Addon(std::unique_ptr<Beverage> beverage) : m_beverage{std::move(beverage)} {}
 
   protected:
     std::unique_ptr<Beverage> m_beverage;
@@ -38,25 +38,25 @@ class Addon : public Beverage {
 class Milk : public Addon {
   public:
     using Addon::Addon;
-    std::string getDescription() override { return m_beverage->getDescription() + " + milk"; }
-    int getCost() override { return m_beverage->getCost() + 7; }
+    std::string getDescription() const override { return m_beverage->getDescription() + " + milk"; }
+    int getCost() const override { return m_beverage->getCost() + 7; }
 };
 
 class Sugar : public Addon {
   public:
     using Addon::Addon;
-    std::string getDescription() override { return m_beverage->getDescription() + " + sugar"; }
-    int getCost() override { return m_beverage->getCost() + 3; }
+    std::string getDescription() const override { return m_beverage->getDescription() + " + sugar"; }
+    int getCost() const override { return m_beverage->getCost() + 3; }
 };
 
 class Honey : public Addon {
   public:
     using Addon::Addon;
-    std::string getDescription() override { return m_beverage->getDescription() + " + honey"; }
-    int getCost() override { return m_beverage->getCost() + 5; }
+    std::string getDescription() const override { return m_beverage->getDescription() + " + honey"; }
+    int getCost() const override { return m_beverage->getCost() + 5; }
 };
 
 int main() {
-    auto beverage = std::make_unique<Honey>(std::make_unique<Milk>(std::make_unique<Tea>()));
+    const auto beverage = std::make_unique<Honey>(std::make_unique<Milk>(std::make_unique<Tea>()));
     std::cout << beverage->getDescription() << " costs " << beverage->getCost() << " uah\n";
 }
