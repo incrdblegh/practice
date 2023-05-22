3d1bfbb8d024144fc0a7150677a766e7310dff2d
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-05-22 09:40:17 +0300

    update facade pattern solution, add comments

--- Design Patterns/Structural Patterns/facade.cpp
+++ Design Patterns/Structural Patterns/facade.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
 #include <memory>
 
+// SubsystemA represents a subsystem that provides two operations: operationA and operationB.
 class SubsystemA {
   public:
-    std::string operationA() const { return "Subsystem A operation A\n"; }
-    std::string operationB() const { return "Subsystem A operation B\n"; }
+    // Returns the result of performing operation A on this subsystem.
+    [[nodiscard]] std::string operationA() const { return "Subsystem A operation A\n"; }
+
+    // Returns the result of performing operation B on this subsystem.
+    [[nodiscard]] std::string operationB() const { return "Subsystem A operation B\n"; }
 };
 
+// SubsystemB represents another subsystem that provides two operations: operationA and operationB.
 class SubsystemB {
   public:
-    std::string operationA() const { return "Subsystem B operation A\n"; }
-    std::string operationB() const { return "Subsystem B operation B\n"; }
+    // Returns the result of performing operation A on this subsystem.
+    [[nodiscard]] std::string operationA() const { return "Subsystem B operation A\n"; }
+
+    // Returns the result of performing operation B on this subsystem.
+    [[nodiscard]] std::string operationB() const { return "Subsystem B operation B\n"; }
 };
 
+// Facade provides a unified interface to the SubsystemA and SubsystemB subsystems.
 class Facade {
   public:
+    // Constructs a Facade object that uses the given SubsystemA and SubsystemB objects.
     Facade(std::unique_ptr<SubsystemA> A, std::unique_ptr<SubsystemB> B)
         : m_subsystemA{std::move(A)}, m_subsystemB{std::move(B)} {}
-    std::string operationA() const { return m_subsystemA->operationA() + m_subsystemB->operationA(); }
-    std::string operationB() const { return m_subsystemA->operationB() + m_subsystemB->operationB(); }
+
+    // Returns the result of performing operation A on both subsystems.
+    [[nodiscard]] std::string operationA() const {
+        return m_subsystemA->operationA() + m_subsystemB->operationA();
+    }
+
+    // Returns the result of performing operation B on both subsystems.
+    [[nodiscard]] std::string operationB() const {
+        return m_subsystemA->operationB() + m_subsystemB->operationB();
+    }
 
   private:
-    std::unique_ptr<SubsystemA> m_subsystemA;
-    std::unique_ptr<SubsystemB> m_subsystemB;
+    // The SubsystemA object used by this Facade object.
+    const std::unique_ptr<SubsystemA> m_subsystemA;
+
+    // The SubsystemB object used by this Facade object.
+    const std::unique_ptr<SubsystemB> m_subsystemB;
 };
 
 int main() {
-    auto facade = std::make_unique<Facade>(std::make_unique<SubsystemA>(), std::make_unique<SubsystemB>());
+    // Create a Facade object that uses a SubsystemA object and a SubsystemB object.
+    const auto facade =
+        std::make_unique<Facade>(std::make_unique<SubsystemA>(), std::make_unique<SubsystemB>());
+
+    // Perform operation A using the Facade object and print the result.
     std::cout << facade->operationA();
+
+    // Perform operation B using the Facade object and print the result.
     std::cout << facade->operationB();
 }
