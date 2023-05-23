a89e3180cf6b1c6e844e9e7664972d6284730416
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-05-23 08:49:41 +0300

    add comments to prototype pattern solution

--- Design Patterns/Creational Patterns/prototype.cpp
+++ Design Patterns/Creational Patterns/prototype.cpp
@@ -2,55 +2,82 @@
 #include <memory>
 #include <unordered_map>
 
+// Abstract base class for shapes
 class Shape {
   public:
+    // Virtual destructor
     virtual ~Shape() = default;
-    virtual std::unique_ptr<Shape> clone() = 0;
-    virtual void draw() = 0;
+
+    // Pure virtual method to clone the shape
+    [[nodiscard]] virtual std::unique_ptr<Shape> clone() = 0;
+
+    // Pure virtual method to draw the shape
+    virtual void draw() const = 0;
 };
 
+// Derived class for circles
 class Circle : public Shape {
   public:
+    // Override the clone method to create a new instance of Circle
     std::unique_ptr<Shape> clone() override { return std::make_unique<Circle>(*this); }
-    void draw() override { std::cout << "Drawing a circle\n"; }
+
+    // Override the draw method to draw a circle
+    void draw() const override { std::cout << "Drawing a circle\n"; }
 };
 
+// Derived class for rectangles
 class Rectangle : public Shape {
   public:
+    // Override the clone method to create a new instance of Rectangle
     std::unique_ptr<Shape> clone() override { return std::make_unique<Rectangle>(*this); }
-    void draw() override { std::cout << "Drawing a rectangle\n"; }
+
+    // Override the draw method to draw a rectangle
+    void draw() const override { std::cout << "Drawing a rectangle\n"; }
 };
 
+// Derived class for triangles
 class Triangle : public Shape {
   public:
+    // Override the clone method to create a new instance of Triangle
     std::unique_ptr<Shape> clone() override { return std::make_unique<Triangle>(*this); }
-    void draw() override { std::cout << "Drawing a triangle\n"; }
+
+    // Override the draw method to draw a triangle
+    void draw() const override { std::cout << "Drawing a triangle\n"; }
 };
 
+// Enum class for shape types
 enum class Shapes {
     CIRCLE,
     RECTANGLE,
     TRIANGLE,
 };
 
+// Class for managing shape prototypes
 class ShapeRegistry final {
   public:
+    // Method to add a shape prototype to the registry
     void addShape(const Shapes shape, std::unique_ptr<Shape> prototype) {
         m_registry[shape] = std::move(prototype);
     }
+
+    // Method to clone a shape from the registry
     std::unique_ptr<Shape> cloneShape(const Shapes shape) {
         auto it = m_registry.find(shape);
         if (it != m_registry.end()) {
-            return m_registry[shape]->clone();
+            return it->second->clone();
         }
         return nullptr;
     }
+
+    // Method to remove a shape prototype from the registry
     void removeShape(const Shapes shape) { m_registry.erase(shape); }
 
   private:
+    // Map to store shape prototypes
     std::unordered_map<Shapes, std::unique_ptr<Shape>> m_registry;
 };
 
+// Client code that uses the ShapeRegistry class
 void client() {
     auto circle = std::make_unique<Circle>();
     auto registry = std::make_unique<ShapeRegistry>();
