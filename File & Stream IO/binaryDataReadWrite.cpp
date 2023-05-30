fa695fb24542c3165f0053a872764358bfc76528
incrdblegh <55416839+incrdblegh@users.noreply.github.com>
2023-05-30 14:26:21 +0300

    update the solution with std::filesystem

--- File & Stream IO/binaryDataReadWrite.cpp
+++ File & Stream IO/binaryDataReadWrite.cpp
@@ -1,13 +1,14 @@
-// Soon to be updated with std::filesystem, as soon as I learn what it is.
-
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 // A function to write binary data to a file.
-template <typename T> void binaryWrite(const std::string& filename, const std::vector<T>& data) {
-    std::ofstream outputFileStream{filename, std::ios::binary};
+template <typename T> void binaryWrite(const fs::path& binaryFile, const std::vector<T>& data) {
+    std::ofstream outputFileStream{binaryFile, std::ios::binary};
     // ofstream object will throw on failure.
     outputFileStream.exceptions(std::ios::failbit | std::ios::badbit);
     // write() accepts a pointer to char, hence we reinterpret_cast.
@@ -15,8 +16,8 @@ template <typename T> void binaryWrite(const std::string& filename, const std::v
 }
 
 // A function to read binary data from a file.
-template <typename T> void binaryRead(const std::string& filename, std::vector<T>& container) {
-    std::ifstream inputFileStream{filename, std::ios::binary};
+template <typename T> void binaryRead(const fs::path& binaryFile, std::vector<T>& container) {
+    std::ifstream inputFileStream{binaryFile, std::ios::binary};
     // ifstream object will throw on failure.
     inputFileStream.exceptions(std::ios::failbit | std::ios::badbit);
     // read() accepts a pointer to char, hence we reinterpret_cast.
@@ -33,10 +34,10 @@ template <typename T> void print(const std::vector<T>& container) {
 
 int main() {
     const std::vector data{1, 2, 3, 4, 5};
-    const std::string filename{"data.bin"};
+    const fs::path binaryFile{"data.bin"};
 
     try {
-        binaryWrite(filename, data);
+        binaryWrite(binaryFile, data);
     } catch (const std::ios_base::failure& exception) {
         std::cout << exception.what() << '\n';
     }
@@ -44,7 +45,7 @@ int main() {
     std::vector<int> newData(5);
 
     try {
-        binaryRead(filename, newData);
+        binaryRead(binaryFile, newData);
     } catch (const std::ios_base::failure& exception) {
         std::cout << exception.what() << '\n';
     }
